9ab3e5c237b22b0f170d6b130df6a3a275655470
EstefSpace <estef.space@gmail.com>
2026-02-26 15:02:53 -0600

    add: connection to mongodb

--- db/connect.go
+++ db/connect.go
@@ -1,5 +1,30 @@
 package db
 
-// To-Do: Implementar conexion a mongodb
+import (
+	"context"
+	"time"
 
-var Client int
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var Client *mongo.Client
+
+func ConnectDB(mongouri string) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(options.Client().ApplyURI(mongouri))
+	if err != nil {
+		return err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return err
+	}
+
+	// que rico
+	Client = client
+	return nil
+}
--- main.go
+++ main.go
@@ -6,20 +6,32 @@ package main
 */
 
 import (
+	"context"
+	"log"
 	"os"
 
+	"estef.link/db"
 	"estef.link/routes"
 	"github.com/gofiber/fiber/v3"
 	"github.com/subosito/gotenv"
 )
 
 func main() {
-	app := fiber.New()
-	err := gotenv.Load()
-	if err != nil {
+	if err := gotenv.Load(); err != nil {
 		panic(err)
 	}
 	port := os.Getenv("PORT")
+	uri := os.Getenv("MONGOURI")
+	if err := db.ConnectDB(uri); err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := db.Client.Disconnect(context.TODO()); err != nil {
+			log.Printf("error disconnecting from MongoDB %v", err)
+		}
+	}()
+	app := fiber.New()
 
 	// iniciando rutas
 	routes.InitRoutes(app)
